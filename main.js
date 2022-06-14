f29a2196ce235e47eb36eef939e50ef645a8997f
Pedro Marques <pdro.marqs2015@hotmail.com>
2022-06-13 23:30:54 -0300

    Setup base do projeto

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    //Cria a janela do app
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    //Carrega o index.html do app
+    win.loadFile('index.html');
+
+    //Habilita o DevTools
+    win.webContents.openDevTools()
+}
+
+// Esse metodo vai ser chamado quando o electron finalizar a inicialização e estiver pronto para criar uma janela.
+// Algumas API's só podem ser usadas depois dessa ação.
+app.whenReady().then(createWindow);
+
+// Sair quando a janela do app for fechada
+app.on('window-all-closed', () => {
+    // No MAC-OS é comum a aplicação continuar aberta até que o usuario sair usando cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // No MAC-OS é comum reacriar uma janela no app quando clica no icone na dock e nao tem outra janela aberta
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
